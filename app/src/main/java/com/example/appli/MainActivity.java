61dadf16c5d502136ba81f4aaffb2e035076a89b
simon <simon.p5207@gmail.com>
2022-01-30 17:48:57 +0100

    ajout bouton dznd lz barre de test et essaie de bare de navigation

--- app/src/main/java/com/example/appli/MainActivity.java
+++ app/src/main/java/com/example/appli/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.appli;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        // Find the toolbar view inside the activity layout
+        Toolbar menu =(Toolbar) findViewById(R.id.bottom_navigation);
+
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.changement_page, menu);
+        return true;
+    }
+
+    public boolean onCreateOptionsMenuNav(Menu menu_nav) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu_nav);
+        return true;
     }
 }
